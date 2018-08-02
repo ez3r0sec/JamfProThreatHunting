bb4164373709b8f614d0ae247dbeea146a626315
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-02 09:41:52 -0400

    Update detectFirefoxExtensions.py

--- detectFirefoxExtensions.py
+++ detectFirefoxExtensions.py
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 # detectFirefoxExtensions.py
 # look for firefox extensions
-# Last Edited: 7/12/18 Julian Thies
+# Last Edited: 8/2/18 Julian Thies
 # -----------------------------------------------------------------------------
 
 ### IMPORTS
@@ -15,42 +15,42 @@ resultsFile = "/tmp/xpi.txt"
 ### FUNCTIONS
 ''' function to write results line by line '''
 def write_to_file(filepath, contents):
-     with open(filepath, 'a') as f:
-          f.write(contents + os.linesep)
+	with open(filepath, 'a') as f:
+		f.write(contents + os.linesep)
 
 ''' take a sha256 hash of found files '''
 def hash_file(filename):
-     bufferSize = 65536
-     sha256Hash = hashlib.sha256()
-     with open(filename, 'rb') as f:
-          while True:
-               data = f.read(bufferSize)
-               if not data: 
-                    break
-          sha256Hash.update(data)
-     hashResult = "{0}".format(sha256Hash.hexdigest())
-     return hashResult
+	bufferSize = 65536
+	sha256Hash = hashlib.sha256()
+	with open(filename, 'rb') as f:
+		while True:
+			data = f.read(bufferSize)
+			if not data: 
+				break
+			sha256Hash.update(data)
+	hashResult = "{0}".format(sha256Hash.hexdigest())
+	return(hashResult)
 
 ''' search for files matching the specified file type'''
 def search_file_type(fileType, searchPath):
-     for root, dirs, files in os.walk(searchPath):
-          for file in files:
-               if file.endswith(fileType):
-                    filePath = os.path.join(root, file)
-                    fileHash = hash_file(filePath)
-                    write_to_file(resultsFile, filePath + " -- SHA256: " + fileHash)
+	for root, dirs, files in os.walk(searchPath):
+		for file in files:
+			if file.endswith(fileType):
+				filePath = os.path.join(root, file)
+				fileHash = hash_file(filePath)
+				write_to_file(resultsFile, filePath + " -- SHA256: " + fileHash)
 
 ''' read the results file for the JSS '''
 def read_result_file(filename):
-     if os.path.exists(filename):
-          with open(filename, 'r') as f:
-               fileContent = f.read()
-          print("<result>" + fileContent + "</result>")
-     else:
-          print("<result>None</result>")
+	if os.path.exists(filename):
+		with open(filename, 'r') as f:
+			fileContent = f.read()
+			print("<result>" + fileContent + "</result>")
+			os.remove(filename)
+	else:
+		print("<result>None</result>")
 
 ### SCRIPT
 search_file_type("XPI", "/")
 read_result_file(resultsFile)
-os.remove(resultsFile)
 # -----------------------------------------------------------------------------
