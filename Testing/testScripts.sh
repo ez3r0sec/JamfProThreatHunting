ad2523becd1bd80789c3568a7959067fa093d2ac
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-16 12:52:59 -0400

    Update testScripts.sh

--- Testing/testScripts.sh
+++ Testing/testScripts.sh
@@ -6,12 +6,12 @@
 # After downloading the repo, ad hoc hunting could be performed on single systems using
 #+this script as well
 # Make sure you input the profile IDs in the detectNewProfiles.sh script before running
-# Last Edited: 7/31/18 Julian Thies
+# Last Edited: 8/16/18 Julian Thies
 # -----------------------------------------------------------------------------
 
 ### SCRIPT
-ls -1 ../*.sh > /tmp/scripts.txt
-ls -1 ../*.py >> /tmp/scripts.txt
+ls -1R ../ | grep 'sh' > /tmp/scripts.txt
+ls -1R ../ | grep 'py' >> /tmp/scripts.txt
 
 cat /tmp/scripts.txt | while read line
 do
